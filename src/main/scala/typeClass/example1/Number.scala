214c57764b882badf181f92bfed614d301f64ebc
konstantinmusienko <konstantin.musienko@sysgears.com>
2019-02-12 12:12:12 +0200

    Add 'minus' operation to the Number trait for 'type class' pattern

--- src/main/scala/typeClass/example1/Number.scala
+++ src/main/scala/typeClass/example1/Number.scala
@@ -4,6 +4,8 @@ package typeClass.example1
 trait Number[A] {
 
   def plus(x: A, y: A): A
+
+  def minus(x: A, y: A): A
 }
 
 object Number {
@@ -14,17 +16,31 @@ object Number {
   */
   implicit object IntNumber extends Number[Int] {
     override def plus(x: Int, y: Int): Int = x + y
+
+    override def minus(x: Int, y: Int): Int = x - y
   }
 
   implicit object DoubleNumber extends Number[Double] {
     override def plus(x: Double, y: Double): Double = x + y
+
+    override def minus(x: Double, y: Double): Double = x - y
   }
 
 }
 
 object Stats {
+
+  /* same as:
+ def plus[A](x: A, y: A)(implicit ev: Number[A]): T = {
+    ev.plus(x, y)
+  }
+ */
   def plus[A: Number](x: A, y: A): A = {
     implicitly[Number[A]].plus(x, y)
+  }
+
+  def minus[A: Number](x: A, y: A): A = {
+    implicitly[Number[A]].minus(x, y)
   }
 }
 
@@ -41,6 +57,12 @@ object Main extends App {
   val res1 = plus(x1, y1)
   val res2 = plus(x2, y2)
 
+  val res3 = minus(x1, y1)
+  val res4 = minus(x2, y2)
+
   println(res1)
   println(res2)
+
+  println(res3)
+  println(res4)
 }
